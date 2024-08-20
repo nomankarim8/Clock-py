59570dcb1d0e6d30552f7813098bf7e878929512
nomankarim8 <karimnoman97@gmail.com>
2024-08-20 06:12:45 -0700

    update Clock.py

--- Clock.py
+++ Clock.py
@@ -15,11 +15,12 @@ def time():
     string = strftime('%H:%M:%S %p %a')
 
     label.config(text=string)
-    
+
     label.after(1000, time)
 
 label = Label(root, font=('ds-digital', 80), background='black', foreground='cyan')
 label.pack(anchor='center')
+
 time()
 
 mainloop()
